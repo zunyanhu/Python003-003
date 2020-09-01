398876f1843b70f29f67e3a443740bce157092a6
huzunyan <15895981485@139.com>
2020-09-01 17:32:36 +0800

    第二周作业批改后修正

--- week02/Maoyan/Maoyan/pipelines.py
+++ week02/Maoyan/Maoyan/pipelines.py
@@ -14,23 +14,24 @@ class MaoyanPipeline(object):
         port = spider.settings.get('MYSQL_PORT', 3306)
         user = spider.settings.get('MYSQL_USER', 'root')
         passwd = spider.settings.get('MYSQL_PASSWORD', '')
-
         self.db_conn = pymysql.connect(host=host, port=port, db=db, user=user, passwd=passwd, charset='utf8')
-        self.db_cur = self.db_conn.cursor()
 
     def process_item(self, item, spider):
+        self.db_cur = self.db_conn.cursor()
         self.insert_db(item)
         return item
 
     def insert_db(self, item):
-        sql = """INSERT INTO maoyanfilm_data(film_name, film_type, release_time) VALUES ('%s', '%s', '%s')""" % (item['film_name'], item['film_type'], item['release_time'])
+        sql = """INSERT INTO maoyanfilm_data(film_name, film_type, release_time) VALUES ('%s', '%s', '%s')""" % (
+        item['film_name'], item['film_type'], item['release_time'])
         try:
             self.db_cur.execute(sql)
+            self.db_conn.commit()
         except Exception as e:
             print(f'发生异常：{e}')
             self.db_conn.rollback()
+        finally:
+            self.db_cur.close()
 
     def close_spider(self, spider):
-        self.db_cur.close()
-        self.db_conn.commit()
         self.db_conn.close()

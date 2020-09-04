36f47a21f8605817f58880caed2616a226c9be92
huzunyan <15895981485@139.com>
2020-09-04 13:59:30 +0800

    增加行为列表输出

--- week03/job_01.py
+++ week03/job_01.py
@@ -8,14 +8,17 @@
 import threading
 import time
 import random
+import queue
+
+q = queue.Queue()
 
 
 class DiningPhilosophers(threading.Thread):
     def __init__(self):
         super(DiningPhilosophers, self).__init__()
         self.lock = threading.Lock()
-        self.scientists = range(1, 6)
-        self.output = []
+        self.philosopher = range(0, 5)
+        self.queue = q
 
     # philosopher 哲学家的编号。
     # pickLeftFork 和 pickRightFork 表示拿起左边或右边的叉子。
@@ -23,12 +26,13 @@ class DiningPhilosophers(threading.Thread):
     # putLeftFork 和 putRightFork 表示放下左边或右边的叉子。
 
     def run(self):
-        self.wants_to_eat()
+        data = self.wants_to_eat()
+        q.put(data)
 
     def wants_to_eat(self):
         behavior_record = []
         self.lock.acquire()
-        behavior_record.append(random.choice(self.scientists))
+        behavior_record.append(random.choice(self.philosopher))
         behavior_flag = random.randrange(1, 4)
         if behavior_flag == 1:
             direction_flag = random.randrange(1, 3)
@@ -51,10 +55,8 @@ class DiningPhilosophers(threading.Thread):
             else:
                 self.put_right_fork(behavior_record)
             self.eat(behavior_record)
-
         self.lock.release()
-        time.sleep(random.randrange(1, 5))
-        print(behavior_record)
+        time.sleep(random.randrange(1, 3))
         return behavior_record
 
     @staticmethod
@@ -75,10 +77,19 @@ class DiningPhilosophers(threading.Thread):
 
     @staticmethod
     def eat(behavior_record):
-        behavior_record.append('2')
+        behavior_record.append('3')
+
+
+def main():
+    out_put = []
+    n = 2
+    for _ in range(n):
+        threads = [DiningPhilosophers() for _ in range(5)]
+        for t in threads:
+            t.start()
+            out_put.append(q.get())
+    print(out_put)
 
 
 if __name__ == "__main__":
-    for i in range(60):
-        t = DiningPhilosophers()
-        t.start()
+    main()

b298a1168b011788f6e50944c65046d51d810c75
胡尊言 <huzunyan@huzunyandeMacBook-Pro.local>
2020-09-06 22:06:06 +0800

    第三周作业提交，2、3未完成

--- week03/job_02_lagou_spider.py
+++ week03/job_02_lagou_spider.py
@@ -9,7 +9,7 @@ from selenium import webdriver
 from multiprocessing import Process
 import requests
 import time
-import lxml
+from lxml import etree
 
 
 def get_lagou_beijing(url, browser):
@@ -20,19 +20,29 @@ def get_lagou_beijing(url, browser):
     submit = browser.find_element_by_id('search_button')
     submit.click()
     time.sleep(3)
-    cookies = browser.get_cookies()
-    browser.close()
+    c = browser.get_cookies()
+    cookies = {}
+    for cookie in c:
+        cookies[cookie['name']] = cookie['value']
     headers = {
-        'Cookie': cookies
+        'authority': 'www.lagou.com',
+        'method': 'GET',
+        'path': '/jobs/list_python%E5%B7%A5%E7%A8%8B%E5%B8%88?labelWords=sug&fromSearch=true&suginput=py',
+        'scheme': 'https',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'max-age=0',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
     }
-    response = requests.get(browser.current_url, headers=headers)
+    print(cookies)
+    response = requests.get(url=browser.current_url, cookies=cookies, headers=headers)
     page_text = response.text
-    # money_list = response.xpath('//span[@class="money"]//span/text()').extract()
-    # bs_info = bs(response.text, 'html.parser')
-    # money_list = bs_info.find_all('span', attrs={'class': 'money'})
-    page_text.xpath()
-    print(money_list)
-
+    selector = etree.HTML(page_text)
+    li_list = selector.xpath('//ul[@class="item_con_list"]/li')
+    print(li_list)
+    browser.close()
 
 
 def get_lagou_shanghai():

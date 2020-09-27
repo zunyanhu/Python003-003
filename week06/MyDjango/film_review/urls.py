ee4e52a00dd6282fec09bf1db81bf9064d8b432f
huzunyan <15895981485@139.com>
2020-09-27 11:43:26 +0800

    作业提交目录错误修改

---
+++ week06/MyDjango/film_review/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/24 14:26
+# @Author  : huzunyan
+# @File    : urls
+# @Software: PyCharm
+# @Contact ： zunyan.hu@gmail.com
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('', views.film_review),
+    url('search/', views.search, name='search'),
+]
---
+++ week06/MyDjango/index/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/25 11:06
+# @Author  : huzunyan
+# @File    : urls
+# @Software: PyCharm
+# @Contact ： zunyan.hu@gmail.com
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.index),
+]
